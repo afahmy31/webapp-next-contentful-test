caf8a0c1879dd89124b048f7c49d8d648178e4cc
AlyElGanzouri <afahmy31@gmail.com>
2018-06-26 16:50:35 +0200

    modify index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { Component } from 'react'
 import data from './../data/person'
 import PersonList from '../components/PersonList'
-import {getcontent} from '../helpers/getcontent.js'
 
 const Index = () => (  
   <div>
